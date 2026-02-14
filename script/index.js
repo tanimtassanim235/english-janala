cc550ef2c8e3d053d6bd97bef566d520ce5408bd
Tanim Tassanim <tanimtassanim235@gmail.com>
2026-02-14 21:45:21 +0600

    learn vacabulary done

--- script/index.js
+++ script/index.js
@@ -1,25 +1,57 @@
 const loadLessons = () => {
-    fetch("https://openapi.programming-hero.com/api/levels/all")
+  fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
-    .then((json) => displayLessons(json?.data))
-}
+    .then((json) => displayLessons(json?.data));
+};
 
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data?.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+     wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+      <h2 class="font-bold text-2xl">${word?.word}</h2>
+      <p class="font-semibold">Meaning / Pronounciation</p>
+
+      <div class="text-2xl font-medium font-bangla">${word?.meaning} / ${word?.pronunciation}</div>
+
+      <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+          <i class="fa-solid fa-circle-info"></i>
+        </button>
+        <button class="btn #1A91FF10 hover:bg-[#1A91FF80]">
+          <i class="fa-solid fa-volume-high "></i>
+        </button>
+      </div>
+    </div>
+    `;
+    wordContainer.append(card);
+  });
+};
 
 const displayLessons = (lessons) => {
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML = "";
-    for(let lesson of lessons)
-    {
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary">
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button onClick="loadLevelWord(${lesson?.level_no})" class="btn btn-outline btn-primary">
               <i class="fa-solid fa-book-open"></i>
               Learn No - ${lesson?.level_no}
         </button>
         `;
 
-        levelContainer.append(btnDiv);
-    }
-}
+    levelContainer.append(btnDiv);
+  }
+};
 
 loadLessons();
